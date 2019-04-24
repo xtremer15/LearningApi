61591adb06556c97e7ef04fb4bef7c89cff0268d
mihai.constantin <mihai_constantin94@yahoo.ro>
2019-04-24 15:26:15 +0300

    Implemeting Bubble Sort

--- src/main/java/BaseClass.java
+++ src/main/java/BaseClass.java
@@ -1,11 +1,79 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class BaseClass {
     public static void main(String[] args) {
 
-        String done = "done";
+
         boolean finished = true;
-        if (done.equals(finished)) {
-            System.out.println("done");
+
+
+        int[] array = {99, 88, 55, 77, 1, 66};
+
+        System.out.print("unsorted data: ");
+        printArray(array);
+
+        System.out.print("ascending order: "); //1,55,66,77,88,99
+        bubble_sort(array);
+
+        printArray(array);
+
+        System.out.print("descending order: "); //99,88,77,66,55,1
+        bubble_sort(array, false);
+
+        printArray(array);
+
+
+    }
+
+
+    private static void bubble_sort(int[] input) {
+        bubble_sort(input, true);
+    }
+
+    private static void bubble_sort(int[] input, boolean ascending) {
+
+        int inputLength = input.length;
+        int temp;
+        boolean is_sorted;
+
+        for (int i = 0; i < inputLength; i++) {
+
+            is_sorted = true;
+
+            for (int j = 1; j < (inputLength - i); j++) {
+
+                if (ascending) {
+                    if (input[j - 1] > input[j]) {
+                        temp = input[j - 1];
+                        input[j - 1] = input[j];
+                        input[j] = temp;
+                        is_sorted = false;
+                    }
+                } else {
+                    if (input[j - 1] < input[j]) {
+                        temp = input[j - 1];
+                        input[j - 1] = input[j];
+                        input[j] = temp;
+                        is_sorted = false;
+                    }
+
+                }
+
+            }
+
+            // is sorted? then break it, avoid useless loop.
+            if (is_sorted) break;
 
         }
+
     }
+
+    private static void printArray(int[] data) {
+        String result = Arrays.stream(data)
+                .mapToObj(String::valueOf)
+                .collect(Collectors.joining(","));
+        System.out.println(result);
+    }
+
 }
