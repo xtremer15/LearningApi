665e1751b3a9ea9cd17221f367f5200673c44469
mihai.constantin <mihai_constantin94@yahoo.ro>
2019-05-03 15:45:43 +0300

    Done it

--- src/main/java/GetRequestGoogleApi.java
+++ src/main/java/GetRequestGoogleApi.java
@@ -24,19 +24,7 @@ public class GetRequestGoogleApi {
                 and().body("results[1].id", equalTo("a97f9fb468bcd26b68a23072a55af82d4b325e0d")).and()
                 .header("server", "scaffolding on HTTPServer2");
 
-        given().
-                param("key", "AIzaSyBXvxsZKeww6vEkG_lGHToVOfTuWbnn6ns ").
-                param("location", "-33.8670522,151.1957362").
-                param("radius", "1500").
-                param("type", "restaurant").
-                param("keyword", "cruise").
-                when().
-                post("/maps/api/place/nearbysearch/json").
-                then().
-                assertThat().statusCode(200).and().contentType(ContentType.JSON).
-                and().body("results[1].name", equalTo("Australian Cruise Group")).
-                and().body("results[1].id", equalTo("a97f9fb468bcd26b68a23072a55af82d4b325e0d")).and()
-                .header("server", "scaffolding on HTTPServer2");
+
 
 
     }
--- src/main/java/PostRequestGoogleApi.java
+++ src/main/java/PostRequestGoogleApi.java
@@ -1,2 +1,43 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
 public class PostRequestGoogleApi {
+    public static void main(String[] args) {
+        RestAssured.baseURI = "http://216.10.245.166";
+
+        given().
+                queryParam("key", "qaclick123").
+                body("{\n" +
+                        "\n" +
+                        "\"location\":{\n" +
+                        "\n" +
+                        "\"lat\" : -38.383494,\n" +
+                        "\n" +
+                        " \"lng\" : 33.427362\n" +
+                        "\n" +
+                        "},\n" +
+                        "\n" +
+                        " \"accuracy\":50,\n" +
+                        "\n" +
+                        " \"name\":\"Frontline house\",\n" +
+                        "\n" +
+                        "\"phone_number\":\"(+91) 983 893 3937\",\n" +
+                        "\n" +
+                        "\"address\" : \"29, side layout, cohen 09\",\n" +
+                        "\n" +
+                        "\"types\": [\"shoe park\",\"shop\"],\n" +
+                        "\n" +
+                        "\"website\" : \"http://google.com\",\n" +
+                        "\n" +
+                        "\"language\" : \"French-IN\"\n" +
+                        "\n" +
+                        "}").
+                when().
+                post("/maps/api/place/add/json").
+                then().statusCode(200).and().contentType(ContentType.JSON).and().
+                body("status", equalTo("OK"));
+    }
 }
