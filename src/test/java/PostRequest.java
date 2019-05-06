35f36bba2fe49de500d5844b6a1d3c3e670af815
mihai.constantin <mihai_constantin94@yahoo.ro>
2019-05-06 16:38:13 +0300

    Adding Changes

--- src/test/java/PostRequest.java
+++ src/test/java/PostRequest.java
@@ -1,5 +1,7 @@
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import io.restassured.path.xml.XmlPath;
+import io.restassured.response.Response;
 
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,14 +12,19 @@ import static org.hamcrest.Matchers.equalTo;
 
 public class PostRequest {
     public static void main(String[] args) throws IOException {
+        Response response;
         RestAssured.baseURI = "http://216.10.245.166";
         String payLoadXML = GenerateStringFromResource("C:\\Users\\mihai.constantin\\Learning\\target\\postData.xml");
-        given().
+        response = given().
                 queryParam("key", "qaclick123").
                 body(payLoadXML).
                 when().
                 post("/maps/api/place/add/xml ").
                 then().statusCode(200).and().contentType(ContentType.XML).extract().response();
+
+        String responseAsStringFromXML = response.asString();
+        System.out.println(responseAsStringFromXML);
+        XmlPath xmlPath = new XmlPath(responseAsStringFromXML);
     }
 
     //Converts an XML file to String format by passing the path from the xml file
